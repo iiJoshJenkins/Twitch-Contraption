cdf6e6382aa67e93b531c4bd03ada9b573bd471a
iiJoshJenkins <joshua.jenkins@live.co.uk>
2018-05-06 09:38:25 +0100

    Update twitch.js

--- js/twitch.js
+++ js/twitch.js
@@ -6,9 +6,10 @@ const params = {
 	client_id : 'me3nb73juxgc9tbs2ehaxi1tkvz6df',
 	redirect_uri : 'https://iijoshjenkins.github.io/Twitch-Contraption/',
 	scope : 'viewing_activity_read openid channel_subscriptions'
-};
+},
+      url = 'https://id.twitch.tv/oauth2/authorize' + '?' + $.param(params);
 
 $(function(){
 	console.log($('.auth-link'));
-	$('.auth-link').attr('href', 'https://id.twitch.tv/oauth2/authorize' + '?' + $.param(params)); 
+	$('.auth-link').attr('href', url); 
 });
