9c95911ecbe31e7a2b86074c4a636bc96eba7455
iiJoshJenkins <joshua.jenkins@live.co.uk>
2018-05-06 09:31:43 +0100

    Updated to redirect instead of request.

--- js/twitch.js
+++ js/twitch.js
@@ -1,18 +1,13 @@
 function getData(channel){
 }
 
+const params = {
+	response_type: 'id_token',
+	client_id : 'me3nb73juxgc9tbs2ehaxi1tkvz6df',
+	redirect_uri : 'https://iijoshjenkins.github.io/Twitch-Contraption/',
+	scope : 'viewing_activity_read openid channel_subscriptions'
+};
 
 $(function(){
-	const params = {
-		response_type: 'id_token',
-		client_id : 'me3nb73juxgc9tbs2ehaxi1tkvz6df',
-		redirect_uri : 'file:///C:/Users/Joshu/Documents/twitchTest/index.html',
-		scope : 'viewing_activity_read openid channel_subscriptions'
-	};
-
-	$.ajax({
-		url : 'https://id.twitch.tv/oauth2/authorize' + '?' + $.param(params)
-	}).done(function(data){
-		console.log(data);
-	});
+	$('.auth-link')[0].attr('href') = 'https://id.twitch.tv/oauth2/authorize' + '?' + $.param(params);
 });
